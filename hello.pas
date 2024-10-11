a6b34702db89f616462a05fe9bc7f04f3b3037ae
Малиновский Максим (ос.) <ritmax074@gmail.com>
2024-10-11 19:48:12 +0300

    Update 1.21.2

---
+++ hello.pas
@@ -0,0 +1,10 @@
+﻿begin
+  println('Введите язык на котором вы говорите?');
+  var n:=readinteger('1-EN 2-RU 3-CN : ');
+  assert(n in 1..3,'Научись читать...');
+  case n of
+    1: print('Hello!');
+    2: print('Привет!');
+    3: print('嗨');
+  end;  
+end.
